b3d1b8f59789c62d0e57c0de2ed286c4df591e0e
ruskays <112562170+ruskays@users.noreply.github.com>
2022-09-05 20:52:41 +0300

    prod new
    prod new

--- static/js/main.js
+++ static/js/main.js
@@ -50,65 +50,98 @@ $(document).ready(function () {
   });
   $('.js-link-hover').on("mouseenter", function () {
     $(this).parent().prev().find('.zagimg').attr('src', $(this).attr('data-back-src'));
-  }); // I hope this over-commenting helps. Let's do this!
-  // Let's use the 'active' variable to let us know when we're using it
+  }); //mobTabs Product
 
-  var active = false; // First we'll have to set up our event listeners
-  // We want to watch for clicks on our scroller
+  var tab = function tab() {
+    var tabNav = document.querySelectorAll('.tabs-nav__item'),
+        // Выбираем элементы навигации табов
+    tabContent = document.querySelectorAll('.tabs-content__item'),
+        // Выбираем самы табы
+    tabName; // Переменная для имени таба
+    // Проходим циклом по каждому элементу навигации и навешиваем обработчик клика, который вызывает функцию selectTabNav
 
-  document.querySelector('.scroller').addEventListener('mousedown', function () {
-    active = true; // Add our scrolling class so the scroller has full opacity while active
+    tabNav.forEach(function (item) {
+      item.addEventListener('click', selectTabNav);
+    });
 
-    document.querySelector('.scroller').classList.add('scrolling');
-  }); // We also want to watch the body for changes to the state,
-  // like moving around and releasing the click
-  // so let's set up our event listeners
+    function selectTabNav() {
+      tabNav.forEach(function (item) {
+        // Удаляем активный класс у всех элементов навигации табов
+        item.classList.remove('is-active');
+      });
+      this.classList.add('is-active'); // Добавляем активный укласс у элемента по которому кликнули
 
-  document.body.addEventListener('mouseup', function () {
-    active = false;
-    document.querySelector('.scroller').classList.remove('scrolling');
-  });
-  document.body.addEventListener('mouseleave', function () {
-    active = false;
-    document.querySelector('.scroller').classList.remove('scrolling');
-  }); // Let's figure out where their mouse is at
+      tabName = this.getAttribute('data-tab-name'); // Получаем имя таба, который нам нужен
 
-  document.body.addEventListener('mousemove', function (e) {
-    if (!active) return; // Their mouse is here...
+      selectTabContent(tabName); // Запускаем функцию, чтобы показать выбранный таб
+    }
 
-    var x = e.pageX; // but we want it relative to our wrapper
+    function selectTabContent(tab) {
+      // Проходим по всем табам и проверяем есть ли у элемента класс, равный имени таба(переменной tabName). Если есть, то добавляем класс активного таба, если нет, то удаляем этот класс
+      tabContent.forEach(function (item) {
+        var classList = item.classList;
+        classList.contains(tab) ? classList.add('is-active') : classList.remove('is-active');
+      });
+    }
+  };
 
-    x -= document.querySelector('.sliderwrapper').getBoundingClientRect().left; // Okay let's change our state
-
-    scrollIt(x);
-  }); // Let's use this function
-
-  function scrollIt(x) {
-    var transform = Math.max(0, Math.min(x, document.querySelector('.sliderwrapper').offsetWidth));
-    document.querySelector('.after').style.width = transform + "px";
-    document.querySelector('.scroller').style.left = transform - 25 + "px";
-  } // Let's set our opening state based off the width, 
-  // we want to show a bit of both images so the user can see what's going on
-
-
-  scrollIt(950); // And finally let's repeat the process for touch events
-  // first our middle scroller...
-
-  document.querySelector('.scroller').addEventListener('touchstart', function () {
-    active = true;
-    document.querySelector('.scroller').classList.add('scrolling');
-  });
-  document.body.addEventListener('touchend', function () {
-    active = false;
-    document.querySelector('.scroller').classList.remove('scrolling');
-  });
-  document.body.addEventListener('touchcancel', function () {
-    active = false;
-    document.querySelector('.scroller').classList.remove('scrolling');
-  });
-  $('.js-slider-prod').slick({
-    dots: true,
-    arrows: false,
-    appendDots: $('.slick-slider-dots')
-  });
+  tab(); // $('.js-slider-prod').slick({
+  //   dots: true,
+  //   arrows: false,
+  //   appendDots: $('.slick-slider-dots'),
+  // });
+  //        // I hope this over-commenting helps. Let's do this!
+  // // Let's use the 'active' variable to let us know when we're using it
+  // let active = false;
+  // // First we'll have to set up our event listeners
+  // // We want to watch for clicks on our scroller
+  // document.querySelector('.scroller').addEventListener('mousedown',function(){
+  //   active = true;
+  //   // Add our scrolling class so the scroller has full opacity while active
+  //   document.querySelector('.scroller').classList.add('scrolling');
+  // });
+  // // We also want to watch the body for changes to the state,
+  // // like moving around and releasing the click
+  // // so let's set up our event listeners
+  // document.body.addEventListener('mouseup',function(){
+  //   active = false;
+  //   document.querySelector('.scroller').classList.remove('scrolling');
+  // });
+  // document.body.addEventListener('mouseleave',function(){
+  //   active = false;
+  //   document.querySelector('.scroller').classList.remove('scrolling');
+  // });
+  // // Let's figure out where their mouse is at
+  // document.body.addEventListener('mousemove',function(e){
+  //   if (!active) return;
+  //   // Their mouse is here...
+  //   let x = e.pageX;
+  //   // but we want it relative to our wrapper
+  //   x -= document.querySelector('.sliderwrapper').getBoundingClientRect().left;
+  //   // Okay let's change our state
+  //   scrollIt(x);
+  // });
+  // // Let's use this function
+  // function scrollIt(x){
+  //     let transform = Math.max(0,(Math.min(x,document.querySelector('.sliderwrapper').offsetWidth)));
+  //     document.querySelector('.after').style.width = transform+"px";
+  //     document.querySelector('.scroller').style.left = transform-25+"px";
+  // }
+  // // Let's set our opening state based off the width, 
+  // // we want to show a bit of both images so the user can see what's going on
+  // scrollIt(950);
+  // // And finally let's repeat the process for touch events
+  // // first our middle scroller...
+  // document.querySelector('.scroller').addEventListener('touchstart',function(){
+  //   active = true;
+  //   document.querySelector('.scroller').classList.add('scrolling');
+  // });
+  // document.body.addEventListener('touchend',function(){
+  //   active = false;
+  //   document.querySelector('.scroller').classList.remove('scrolling');
+  // });
+  // document.body.addEventListener('touchcancel',function(){
+  //   active = false;
+  //   document.querySelector('.scroller').classList.remove('scrolling');
+  // });
 });
