719222fe2b0aa31dbc6c23bd280d1edd5a7d4094
ruskays <112562170+ruskays@users.noreply.github.com>
2022-12-16 19:16:32 +0300

    addnew
    addnew

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,14 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function () {
-  //preloader
+
+  //активный класс lang
+  $('.lang-item').on('click', function() {
+    $('.lang-item').removeClass('is--active');
+    $(this).addClass('is--active')
+  })
+
+  //slider banner
   $('.js-banner').slick({
     fade: true,
     nextArrow: '.js-banner__next',
@@ -9,14 +16,22 @@ document.addEventListener('DOMContentLoaded', function () {
     dots: true,
     appendDots: $('.js-banner-dots')
   });
-  var image = document.getElementsByClassName('paralax-img');
-  new simpleParallax(image, {
-    scale: 1.3,
-    delay: .6,
-    transition: 'cubic-bezier(0,0,0,1)'
-  }); 
 
-  var sandwichToggle = function sandwichToggle() {
+
+  //паралакс 
+  let paralaxImg = () => {
+    var image = document.getElementsByClassName('paralax-img');
+    new simpleParallax(image, {
+      scale: 1.3,
+      delay: .6,
+      transition: 'cubic-bezier(0,0,0,1)'
+    }); 
+
+  }
+  paralaxImg()
+  
+  //sendwich
+  let sandwichToggle = function sandwichToggle() {
     // Выбираем элементы, которые нам нужны. В примере мы ищем элементы с классом "sandwich"
     var sandwichElements = document.querySelectorAll('.sandwich'); // Проходим циклом по всем эдементам и на каждый элемент вешаем слушателя, который по клику будет переключать класс
 
